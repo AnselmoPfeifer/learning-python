9eb477a1b1851c62d914fbbfab85345ec6de08eb
Anselmo <anspfeifer@gmail.com>
2018-01-15 00:21:19 -0200

    Mais estudos sobre dictionary usando python

--- Monitoring/monitoring.py
+++ Monitoring/monitoring.py
@@ -1,20 +1,27 @@
 import json
-import sys
 import os
 
-class Host:
 
+class Host:
     def __init__(self, name, url, notification):
         self.name = name
         self.url = url
         self.notification = notification
 
-list_of_hosts = []
+
+def create_json_file():
+    import json
+    hosts = {}
+    hosts['hosts'] = []
+    json = json.dumps(hosts)
+    f = open("hosts.json", "w")
+    f.write(json)
+    f.close()
 
 
 def check_list_of_host():
-    file_name = 'hosts.json'
-    if not os.path.exists('hosts.json') and not list_of_hosts:
+    lista= json.loads(open('hosts.json').read())
+    if not os.path.exists('hosts.json') and not lista['hosts']:
         print 'Deseja cadastrar novas urls?'
         res = raw_input()
         if res == 'Y':
@@ -36,9 +43,6 @@ def check_list_of_host():
             new_file.write(list_of_hosts)
             new_file.close()
 
-            new_file = open('hosts.json', 'r')
-            new_file.read()
-
     else:
         list_of_url = json.loads(open(file_name).read())
 
@@ -46,7 +50,8 @@ def check_list_of_host():
 
 
 
-check_list_of_host()
+
+#check_list_of_host()
 # host = Host(name='Test', url='https://www.anselmopfeifer.com', notification=False)
 
 
